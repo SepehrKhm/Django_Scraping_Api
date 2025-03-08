ed5255e06d716850422f82cffa6eef9f4b35b3ed
Sepehr <152654139+SepehrKhm@users.noreply.github.com>
2025-03-08 10:55:20 +0330

    Delete scraper directory

--- scraper/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ScrapeTask
-
-class ScrapeTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ScrapeTask
-        fields = '__all__'
