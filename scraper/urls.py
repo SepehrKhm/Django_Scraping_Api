2a5ae691b9d55ea1939ea7c2acbf35e6fba18173
Sepehr <152654139+SepehrKhm@users.noreply.github.com>
2025-03-09 11:50:53 +0330

    Delete scraper directory

--- scraper/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ScrapeTaskCreateView, ScrapeTaskDetailView, ScrapeTaskListView
-
-urlpatterns = [
-    path('scrape/', ScrapeTaskCreateView.as_view(), name='scrape-create'),
-    path('scrape/<int:pk>/', ScrapeTaskDetailView.as_view(), name='scrape-detail'),
-    path('scrape/list/', ScrapeTaskListView.as_view(), name='scrape-list'),
-]
