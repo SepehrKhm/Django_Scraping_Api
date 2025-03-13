5a891a11d3db4e2f7005fe89460ee32605d20b77
Sepehr <152654139+SepehrKhm@users.noreply.github.com>
2025-03-13 16:49:01 +0330

    Update urls.py

--- scraping_project/urls.py
+++ scraping_project/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+
 from django.urls import path, include
 
+
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),
+
     path('api/', include('scraper.urls')),
+
+    path('api-auth/', include('rest_framework.urls')),
+
 ]
